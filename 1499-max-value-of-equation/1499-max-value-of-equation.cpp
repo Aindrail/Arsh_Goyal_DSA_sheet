e629af14b32ff1d321c0f2350a327bfd77fba9ce
Aindrail <77523404+Aindrail@users.noreply.github.com>
2022-06-28 11:28:41 +0530

    Time: 551 ms (18.03%), Space: 92.4 MB (46.93%) - LeetHub

--- 1499-max-value-of-equation/1499-max-value-of-equation.cpp
+++ 1499-max-value-of-equation/1499-max-value-of-equation.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int findMaxValueOfEquation(vector<vector<int>>& pts, int k) {
-        priority_queue<pair<int, int>> pq; // max-heap
-        pq.push({pts[0][1]-pts[0][0],pts[0][0]});
-        int ans= INT_MIN;
-        for(int i=1;i<pts.size();i++) {
-            int sum = pts[i][0]+pts[i][1];
-            while(!pq.empty() && pts[i][0]-pq.top().second>k) pq.pop();
-            if(!pq.empty())ans = max(ans,sum+pq.top().first);
-            pq.push({pts[i][1]-pts[i][0],pts[i][0]});
-            
-        }
-       
-        return ans;
+    int findMaxValueOfEquation(vector<vector<int>>& points, int k) {
+        priority_queue<pair<int, int>> q;	// {y - x, x}
+            int ans = INT_MIN;
+            for (const auto &p: points)
+            {
+                const int x = p[0],
+                    y = p[1];
+                while (!q.empty() && x - q.top().second > k) q.pop();
+                if (!q.empty())
+                    ans = max(ans, x + y + q.top().first);
+                q.emplace(y - x, x);
+            }
+            return ans;
     }
 };
