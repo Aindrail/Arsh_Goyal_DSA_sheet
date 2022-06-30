aed53b06c50d44193b439a77fbdc9957a0cf6c8c
Aindrail <77523404+Aindrail@users.noreply.github.com>
2022-06-30 19:05:30 +0530

    Time: 630 ms (5.02%), Space: 104 MB (75.41%) - LeetHub

--- 381-insert-delete-getrandom-o1-duplicates-allowed/381-insert-delete-getrandom-o1-duplicates-allowed.cpp
+++ 381-insert-delete-getrandom-o1-duplicates-allowed/381-insert-delete-getrandom-o1-duplicates-allowed.cpp
@@ -1,37 +1,46 @@
 class RandomizedCollection {
 public:
-    vector<int> v;
-    vector<int>::iterator it;
+    /** Initialize your data structure here. */
     RandomizedCollection() {
-        v.clear();
+        
     }
     
+    /** Inserts a value to the collection. Returns true if the collection did not already contain the specified element. */
     bool insert(int val) {
-        if(v.empty()){
-            v.push_back(val);
-            return true;
-        }
-        bool chk = true;
-        it = lower_bound(v.begin(), v.end(), val);
-        if(it != v.end() && *it == val) chk = false;
-        v.insert(it, val);
-        return chk;
+        auto result = m.find(val) == m.end();
+        
+        m[val].push_back(nums.size());
+        nums.push_back(pair<int, int>(val, m[val].size() - 1));
+        
+        return result;
     }
     
+    /** Removes a value from the collection. Returns true if the collection contained the specified element. */
     bool remove(int val) {
-        it = lower_bound(v.begin(), v.end(), val);
-        if(it != v.end() && *it == val){
-            v.erase(it);
-            return true;
+        auto result = m.find(val) != m.end();
+        if(result)
+        {
+            auto last = nums.back();
+            m[last.first][last.second] = m[val].back();
+            nums[m[val].back()] = last;
+            m[val].pop_back();
+            if(m[val].empty()) m.erase(val);
+            nums.pop_back();
         }
-        return false;
+        return result;
     }
     
+    /** Get a random element from the collection. */
     int getRandom() {
-        int tmp = rand() % v.size();
-        return v[tmp];
+        return nums[rand() % nums.size()].first;
     }
+private:
+    vector<pair<int, int>> nums;
+    unordered_map<int, vector<int>> m;
 };
+
+
+
 
 /**
  * Your RandomizedCollection object will be instantiated and called as such:
