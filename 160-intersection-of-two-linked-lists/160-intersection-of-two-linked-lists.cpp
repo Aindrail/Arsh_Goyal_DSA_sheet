5c04527fb43986a993bb9c3d7e7e8bc440678cbd
Aindrail <77523404+Aindrail@users.noreply.github.com>
2022-07-05 22:43:01 +0530

    Time: 667 ms (6.45%), Space: 14.6 MB (31.51%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -9,45 +9,21 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-//     ListNode* d1 = head1;
-//     ListNode* d2 = head2;
-    
-//     while(d1 != d2) {
-//         d1 = d1 == NULL? head2:d1->next;
-//         d2 = d2 == NULL? head1:d2->next;
-//     }
-    
-//     return d1;
-//     }
-        stack<ListNode *> sA, sB;
-        ListNode *ptrA, *ptrB, *ptrI;
-        ptrA = headA; ptrB = headB;
-        while(ptrA){
-            sA.push(ptrA);
-            ptrA = ptrA->next;
-        }
-        while(ptrB){
-                sB.push(ptrB);
-            ptrB = ptrB->next;
-        }
-        if(sB.top() != sA.top()) return NULL;
-        // printf("A B %d %d\n", sA.top(), sB.top());
-        //sA.pop(); sB.pop();
-        int nA = sA.size(); 
-        int nB = sB.size();
-        int n = min(nA, nB);
-        //printf("%d %d %d\n", nA, nB, n);
-        for(int i=1; i<=n; i++){
-            if(sA.top() == sB.top()){
-                //printf("A B %d %d\n", sA.top(), sB.top());
-                ptrI = sA.top();
-                sA.pop();
-                sB.pop();
+        if(headA==NULL || headB==NULL) return NULL;
+        ListNode * A = headA, *B = headB;
+        while(A!=B){
+            if(A==NULL){
+                A=headA;
+            }else{
+               A=A->next;  
             }
-            else {
-                break;
+            if(B==NULL){
+                B=headB;
+
             }
+           else
+            B=B->next;
         }
-        return(ptrI);
+        return A;
     }
 };
