bf2b8f3fd57011236ea7ba282c7d681bab032c4f
Aindrail <77523404+Aindrail@users.noreply.github.com>
2022-07-21 21:49:15 +0530

    Time: 3 ms (99.31%), Space: 9.2 MB (35.90%) - LeetHub

---
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int cycleStart = -1;
+bool *cycle, *vis, found{};
+void dfs(vector<int> graph[], int cur, int par = -1) {
+	if(vis[cur]) { cycleStart = cur; found = true; return; }   
+	vis[cur] = true;                                        
+	for(auto child : graph[cur]) {                          
+		if(child == par) continue;                          
+		if(!found) dfs(graph, child, cur);     
+		if(cycleStart != -1) cycle[cur]=true;  
+		if(cur == cycleStart) { cycleStart = -1; return; } 
+	}
+}
+vector<int> findRedundantConnection(vector<vector<int>>& e) {
+	int n = size(e);
+	vector<int> graph[n+1];
+	cycle = (bool*)calloc(n+1, sizeof(bool));
+    vis = (bool*)calloc(n+1, sizeof(bool));
+	for(auto& edge : e) graph[edge[0]].push_back(edge[1]), graph[edge[1]].push_back(edge[0]);
+	dfs(graph, 1);   
+	for(int i = n-1; ~i; i--)
+		if(cycle[e[i][0]] && cycle[e[i][1]]) return e[i];  
+	return { };    
+}
+};
